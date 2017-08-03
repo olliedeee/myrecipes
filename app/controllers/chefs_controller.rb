8904a8a092776df8ce42d36caec6a05162e24d81
Ollie <oliverbucks@gmail.com>
2017-08-03 23:07:09 +0000

    Added loads of stuff and authentication and sign ups, login broken

---
+++ app/controllers/chefs_controller.rb
@@ -0,0 +1,51 @@
+class ChefsController < ApplicationController
+  
+  def index
+      @chefs = Chef.paginate(page: params[:page], per_page: 4)
+  end
+  
+  def new
+    @chef = Chef.new
+  end
+  
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = "Account created successfully"
+      redirect_to recipes_path
+    else
+      render 'new'
+      
+    end  
+  end
+  
+  def edit
+    @chef = Chef.find(params[:id])
+  end
+  
+  def update
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "Your profile has been updated successfully"
+      redirect_to recipes_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+   @chef = Chef.find(params[:id])
+   @recipes = @chef.recipes.paginate(page: params[:page], per_page: 3)
+  end
+  
+  
+  private 
+
+  def chef_params
+    params.require(:chef).permit(:chefname, :email, :password)
+  end
+  
+  
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
+ 
  root 'pages#home'
 
  get '/home',to: 'pages#home'
  
  
 resources :recipes do
- member do
- post 'like'
- 
- end
+   member do
+      post 'like'
+  end
 end
 
+resources :chefs, except: [:new, :destroy]
 
+get '/register', to: 'chefs#new'
 
-
+get '/login', to: "logins#new"
+post '/login', to: "logins#create"
+get '/logout', to: "logins#destroy"
  
 end
